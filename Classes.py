7ff4545345e64acefffa085835e58307cacd1951
Jonathan Torres <jtorres_01@yahoo.com>
2025-11-13 11:36:09 -0500

    Adding Classes

---
+++ Classes.py
@@ -0,0 +1,29 @@
+class Persons:
+
+#
+#Practice creating classes and methods
+#
+    population = 0
+
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        
+    
+    
+    def info(self) -> str:
+        return f"My name is {self.name} and I am {self.age} years old"
+
+
+person1 = Persons("Mary",56)
+person2 = Persons("Manny",13)
+person3 = Persons("Willy",20)
+
+print(person1.info())
+print()
+person1.age = 23
+person1.name = "Jonathan"
+print(f"Deleting {person2.name} the {person2.spieces}")
+del person2
+
+print(person1.info())
